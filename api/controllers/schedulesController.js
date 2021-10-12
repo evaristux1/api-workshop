5d44079e9745ab6a9eb57d989544af913ce4f4d8
Gabriel Bezerra <gabrielbrodrigues1@gmail.com>
2021-10-12 19:24:23 -0300

    add service to check if user is a instructor

--- api/controllers/schedulesController.js
+++ api/controllers/schedulesController.js
@@ -1,7 +1,20 @@
 const DateLower = require('../errors/DateLower');
 const UserWithoutPermission = require('../errors/UserWithoutPermission')
-
+const {schedulesServices} = require('../services');
+const {usersServices} = require('../services');
 class SchedulesController{
+
+    static async schedulesTest(req, res) {
+        try{
+            await usersServices.isUseraInstructor(req);
+            res.end();
+
+
+        }catch(error){
+            return res.status(403).json({ message: error.message });
+
+        }
+    }
 
 
 }
