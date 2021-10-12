ab944611dc4797b53a0e3f966f71a899aac60641
ftxdante <blog.anutrof@gmail.com>
2021-10-12 08:42:48 -0300

    Repetead code from Services was deleted

--- api/services/services.js
+++ api/services/services.js
@@ -11,15 +11,6 @@ class Services {
   async findOneRecord(where = {}) {
     return database[this.modelName].findOne({ where: { ...where } });
   }
-
-  async getAllRecords() {
-    return database[this.modelName].findAll();
-  }
-  // Estilo de where esperado findOneRecord({where: {id: id}})
-  async findOneRecord(where = {}) {
-    return database[this.modelName].findOne({ where: { ...where } });
-  }
-
   async createARecord(data) {
     return database[this.modelName].create(data);
   }
@@ -31,33 +22,7 @@ class Services {
   async deleteRecord(where) {
     return await database[this.modelName].destroy({ where: { ...where } });
   }
-  async createARecord(data) {
-    return database[this.modelName].create(data);
-  }
-  // Estilo de where esperado updateARecord({where: {id: id}})
-  async updateARecord(data, where) {
-    return database[this.modelName].update(data, where);
-  }
-  // Estilo de where esperado deleteRecord({where: {id: id}})
-  async deleteRecord(where) {
-    return await database[this.modelName].destroy(where);
-    // Estilo de where esperado findOneRecord({where: {id: id}})
-  }
-    async createARecord(data){
-        return database[this.modelName].create(data);
-    }
-    // Estilo de where esperado updateARecord({where: {id: id}})
-    async updateARecord(data, where){
-        return database[this.modelName].update(data, {where:{...where}})
-    }
-    // Estilo de where esperado deleteRecord({where: {id: id}})
-    async deleteRecord(where){
-        return await database[this.modelName].destroy({where:{...where}});
-    }
-
-    async checkUserByToken(req, res){
-        return await database[this.modelName].destroy({where:{...where}});
-    }
+ 
 }
 
 module.exports = Services;
