86bdb1a3ec95c71367f1652b3da4739e5d8157e7
Gabriel Bezerra <gabrielbrodrigues1@gmail.com>
2021-10-13 14:15:12 -0300

    pre-finished schedules post

--- api/controllers/schedulesController.js
+++ api/controllers/schedulesController.js
@@ -17,7 +17,7 @@ class SchedulesController{
 
     static async createSchedule(req, res){
         let data = req.body;
-        data.userId = req.idUserToken;
+        data.instructorId = req.idUserToken;
 
         try{
             await usersServices.isUseraInstructor(req);
