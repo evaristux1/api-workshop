94e09f2642b08565f25652d4f336e3c8447d6ee9
Gabriel Evaristo <gabrielevaristovcp@gmail.com>
2021-10-14 09:08:29 -0300

    create custom query service

--- api/routes/themesRoute.js
+++ api/routes/themesRoute.js
@@ -6,5 +6,6 @@ const router = Router();
 router.post('/themes', Middlewares.tokenValidade, ThemesController.createATheme);
 router.get('/themes?', ThemesController.getAllThemes);
 router.get('/themes/:id', ThemesController.getThemeById);
+router.patch("/themes/:id", Middlewares.tokenValidade, ThemesController.updateATheme);
 
 module.exports = router;
